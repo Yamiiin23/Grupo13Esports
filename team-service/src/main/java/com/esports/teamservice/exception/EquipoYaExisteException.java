6c8aa3cd987c79d9db51245d9daf06553dc1f3de
Yamiiin23 <ya.lodder@duocuc.cl>
2026-05-24 15:22:45 -0400

    Add files via upload

---
+++ team-service/src/main/java/com/esports/teamservice/exception/EquipoYaExisteException.java
@@ -0,0 +1,11 @@
+package com.esports.teamservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class EquipoYaExisteException extends RuntimeException {
+    public EquipoYaExisteException(String nombre) {
+        super("Ya existe un equipo con el nombre: " + nombre);
+    }
+}
