4a8c9c6b547502d77edc0774f690ac659578c7fe
yas <ya.lodder@duocuc.cl>
2026-06-08 18:48:24 -0400

    Cambios de la primera guía de 3.1

--- match-service/src/main/java/com/esports/match_service/controller/PartidaController.java
+++ match-service/src/main/java/com/esports/match_service/controller/PartidaController.java
@@ -2,6 +2,12 @@ package com.esports.match_service.controller;
 
 import com.esports.match_service.dto.PartidaDTO;
 import com.esports.match_service.service.PartidaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +19,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/partidas")
+//cambio de guia 1
+@Tag(name = "Partidas", description= "Operaciones relacionadas con la gestión y calendarizacion de enfrentamiento")
 public class PartidaController {
 
     private static final Logger log = LoggerFactory.getLogger(PartidaController.class);
@@ -23,6 +31,13 @@ public class PartidaController {
     }
 
     @PostMapping
+    //cambio de guia 1
+    @Operation(summary = "Crear un nuevo enfrentamiento", description = "Registra una partida pendiente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Partida creada exitosamente",
+                    content = @Content(schema = @Schema(implementation = PartidaDTO.Response.class))),
+            @ApiResponse(responseCode = "400", description = "Datos de entrada invalidos o error de validacion logica.", content = @Content)
+    })
     public ResponseEntity<PartidaDTO.Response> crearPartida(
             @Valid @RequestBody PartidaDTO.Request request) {
         log.info("[match-service] POST /api/v1/partidas - registrando nuevo enfrentamiento");
@@ -31,6 +46,9 @@ public class PartidaController {
     }
 
     @GetMapping
+    //otro cambio de la guia 1
+    @Operation(summary = "Listar partidas", description = "Obtiene el listado completo de enfrentamientos, de ser posible filtrado por id de torneo")
+    @ApiResponse(responseCode = "200", description = "Lista de partidas obtenida con éxito")
     public ResponseEntity<List<PartidaDTO.Response>> listarPartidas(
             @RequestParam(required = false) Long torneoId) {
         log.info("[match-service] GET /api/v1/partidas - filtro torneoId={}", torneoId);
@@ -39,6 +57,12 @@ public class PartidaController {
     }
 
     @GetMapping("/{id}")
+    //otro cambio más
+    @Operation(summary = "Obtener una partida por ID", description = "Buscar detalles de un enfrentamiento usando su ID")
+    @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Partida encontrada"),
+    @ApiResponse(responseCode = "404", description = "No se encontró el enfrentamiento con el ID proporcionado", content = @Content)
+    })
     public ResponseEntity<PartidaDTO.Response> buscarPorId(@PathVariable Long id) {
         log.info("[match-service] GET /api/v1/partidas/{}", id);
         PartidaDTO.Response response = partidaService.buscarPorId(id);
@@ -46,6 +70,13 @@ public class PartidaController {
     }
 
     @PutMapping("/{id}/resultado")
+    //otro cambio
+    @Operation(summary = "Actualizar resultado y estado de la partida", description = "Permite registrar el marcador final de un encuentro y modificar su estado")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Marcador actualizado correctamente"),
+            @ApiResponse(responseCode = "400", description = "Datos de resultado invalidos"),
+            @ApiResponse(responseCode = "404", description = "Partida no encontrada")
+    })
     public ResponseEntity<PartidaDTO.Response> actualizarResultado(
             @PathVariable Long id,
             @Valid @RequestBody PartidaDTO.UpdateResultRequest request) {
@@ -55,6 +86,12 @@ public class PartidaController {
     }
 
     @DeleteMapping("/{id}")
+    //ultimo cambio
+    @Operation(summary = "Eliminar un enfrentamiento", description = "Remueve fisicamente el registro de la partida del sistema con su ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Partida eliminada con éxito."),
+            @ApiResponse(responseCode = "404", description = "La partida no existe")
+    })
     public ResponseEntity<Void> eliminarPartida(@PathVariable Long id) {
         log.info("[match-service] DELETE /api/v1/partidas/{}", id);
         partidaService.eliminarPartida(id);
