9719ce953d97142dab5685a93a52dfb383647ed7
nicolaslara <nic.larag@duocuc.cl>
2026-06-10 09:54:37 -0400

    prueba

--- auth-service/src/main/java/com/esports/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/esports/auth_service/controller/AuthController.java
@@ -15,7 +15,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+// hola
 @RestController
 @RequestMapping("/api/v1/auth")
 //cambio guia 1
