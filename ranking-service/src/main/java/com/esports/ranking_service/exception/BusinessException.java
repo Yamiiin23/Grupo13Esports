42284a93997b909783aab81bf830b30612000eea
yas <ya.lodder@duocuc.cl>
2026-06-16 12:20:36 -0400

    bussinesException cambiado

--- ranking-service/src/main/java/com/esports/ranking_service/exception/BusinessException.java
+++ ranking-service/src/main/java/com/esports/ranking_service/exception/BusinessException.java
@@ -1,16 +1,14 @@
 package com.esports.ranking_service.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public abstract class BusinessException extends RuntimeException {
     private final HttpStatus status;
 
     public BusinessException(String message, HttpStatus status) {
         super(message);
         this.status = status;
-    }
-
-    public HttpStatus getStatus() {
-        return status;
     }
 }
