b42f4181de5c9ea3e17c39451f660e2abcf4cc0f
yas <ya.lodder@duocuc.cl>
2026-06-16 11:38:48 -0400

    arreglo CRUD puro

--- tournament-service/src/main/java/com/esports/tournament_service/model/Torneo.java
+++ tournament-service/src/main/java/com/esports/tournament_service/model/Torneo.java
@@ -22,11 +22,33 @@ public class Torneo {
     @Column(nullable = false)
     private Long gameId;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private EstadoTorneo estado;
+
+    @Column(nullable = false)
+    private Integer maxParticipantes;
+
+    @Column(nullable = false)
+    private Integer participantesActuales;
+
+    @Column(length = 300)
+    private String motivoAnulacion;
+
     @Column(nullable = false, updatable = false)
     private LocalDateTime fechaCreacion;
 
     @PrePersist
     public void prePersist() {
         this.fechaCreacion = LocalDateTime.now();
+        if (this.estado == null) this.estado == EstadoTorneo.INSCRIPCION;
+        if this.participantesActuales == null) this.participantesActuales = 0;
+        if (this.maxParticipantes == null) this.maxParticipantes = 16;
+    }
+    public enum EstadoTorneo {
+        INSCRIPCION,
+        EN_CURSO,
+        FINALIZADO,
+        ANULADO
     }
 }
